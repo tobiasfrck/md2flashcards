43121d9a417696472103538f4bb4f0f690484859
tobiasf <30748375+tobiasfrck@users.noreply.github.com>
2023-06-22 16:28:06 +0200

    basic filedialog stuff

--- md2flashcards/Main.Designer.vb
+++ md2flashcards/Main.Designer.vb
@@ -22,71 +22,79 @@ Partial Class Form1
     'Do not modify it using the code editor.
     <System.Diagnostics.DebuggerStepThrough()>
     Private Sub InitializeComponent()
-        Button1 = New Button()
+        btnLoadMD = New Button()
         Label1 = New Label()
         Label2 = New Label()
-        Button2 = New Button()
-        Button3 = New Button()
+        btnSaveHTML = New Button()
+        btnAplySettings = New Button()
         Label3 = New Label()
         Button4 = New Button()
         Button5 = New Button()
+        opnFileDiag = New OpenFileDialog()
+        savFileDiag = New SaveFileDialog()
         SuspendLayout()
         ' 
-        ' Button1
+        ' btnLoadMD
         ' 
-        Button1.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
-        Button1.AutoSizeMode = AutoSizeMode.GrowAndShrink
-        Button1.Location = New Point(12, 37)
-        Button1.Name = "Button1"
-        Button1.Size = New Size(307, 35)
-        Button1.TabIndex = 0
-        Button1.Text = "Open Markdown-File"
-        Button1.UseVisualStyleBackColor = True
+        btnLoadMD.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        btnLoadMD.AutoSizeMode = AutoSizeMode.GrowAndShrink
+        btnLoadMD.Location = New Point(10, 30)
+        btnLoadMD.Margin = New Padding(2)
+        btnLoadMD.Name = "btnLoadMD"
+        btnLoadMD.Size = New Size(246, 28)
+        btnLoadMD.TabIndex = 0
+        btnLoadMD.Text = "Open Markdown-File"
+        btnLoadMD.UseVisualStyleBackColor = True
         ' 
         ' Label1
         ' 
         Label1.AutoSize = True
-        Label1.Location = New Point(12, 9)
+        Label1.Location = New Point(10, 7)
+        Label1.Margin = New Padding(2, 0, 2, 0)
         Label1.Name = "Label1"
-        Label1.Size = New Size(153, 25)
+        Label1.Size = New Size(126, 20)
         Label1.TabIndex = 1
         Label1.Text = "Markdown - Load"
         ' 
         ' Label2
         ' 
         Label2.AutoSize = True
-        Label2.Location = New Point(12, 86)
+        Label2.Location = New Point(10, 69)
+        Label2.Margin = New Padding(2, 0, 2, 0)
         Label2.Name = "Label2"
-        Label2.Size = New Size(112, 25)
+        Label2.Size = New Size(93, 20)
         Label2.TabIndex = 2
         Label2.Text = "HTML - Save"
         ' 
-        ' Button2
+        ' btnSaveHTML
         ' 
-        Button2.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
-        Button2.Location = New Point(12, 114)
-        Button2.Name = "Button2"
-        Button2.Size = New Size(307, 34)
-        Button2.TabIndex = 3
-        Button2.Text = "Save HTML-Flashcards"
-        Button2.UseVisualStyleBackColor = True
+        btnSaveHTML.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        btnSaveHTML.Location = New Point(10, 91)
+        btnSaveHTML.Margin = New Padding(2)
+        btnSaveHTML.Name = "btnSaveHTML"
+        btnSaveHTML.Size = New Size(246, 27)
+        btnSaveHTML.TabIndex = 3
+        btnSaveHTML.Text = "Save HTML-Flashcards"
+        btnSaveHTML.UseVisualStyleBackColor = True
         ' 
-        ' Button3
+        ' btnAplySettings
         ' 
-        Button3.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
-        Button3.Location = New Point(12, 213)
-        Button3.Name = "Button3"
-        Button3.Size = New Size(307, 34)
-        Button3.TabIndex = 5
-        Button3.Text = "Apply Settings"
-        Button3.UseVisualStyleBackColor = True
+        btnAplySettings.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
+        btnAplySettings.Location = New Point(10, 170)
+        btnAplySettings.Margin = New Padding(2)
+        btnAplySettings.Name = "btnAplySettings"
+        btnAplySettings.Size = New Size(246, 27)
+        btnAplySettings.TabIndex = 5
+        btnAplySettings.Text = "Apply Settings"
+        btnAplySettings.UseVisualStyleBackColor = True
         ' 
         ' Label3
         ' 
         Label3.AutoSize = True
-        Label3.Location = New Point(12, 250)
+        Label3.Location = New Point(10, 200)
+        Label3.Margin = New Padding(2, 0, 2, 0)
         Label3.Name = "Label3"
-        Label3.Size = New Size(79, 25)
+        Label3.Size = New Size(66, 20)
         Label3.TabIndex = 7
         Label3.Text = "Learning"
         ' 
@@ -94,9 +102,10 @@ Partial Class Form1
         ' 
         Button4.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
         Button4.AutoSizeMode = AutoSizeMode.GrowAndShrink
-        Button4.Location = New Point(12, 278)
+        Button4.Location = New Point(10, 222)
+        Button4.Margin = New Padding(2)
         Button4.Name = "Button4"
-        Button4.Size = New Size(307, 35)
+        Button4.Size = New Size(246, 28)
         Button4.TabIndex = 6
         Button4.Text = "Single Player - Learning"
         Button4.UseVisualStyleBackColor = True
@@ -105,39 +114,51 @@ Partial Class Form1
         ' 
         Button5.Anchor = AnchorStyles.Top Or AnchorStyles.Left Or AnchorStyles.Right
         Button5.AutoSizeMode = AutoSizeMode.GrowAndShrink
-        Button5.Location = New Point(12, 319)
+        Button5.Location = New Point(10, 255)
+        Button5.Margin = New Padding(2)
         Button5.Name = "Button5"
-        Button5.Size = New Size(307, 35)
+        Button5.Size = New Size(246, 28)
         Button5.TabIndex = 6
         Button5.Text = "COOP - Learning"
         Button5.UseVisualStyleBackColor = True
         ' 
+        ' opnFileDiag
+        ' 
+        opnFileDiag.Filter = "Markdown-Files|*.md|All Files|*.*"
+        ' 
+        ' savFileDiag
+        ' 
+        savFileDiag.Filter = "HTML-Files|*.html|All Files|*.*"
+        savFileDiag.RestoreDirectory = True
+        ' 
         ' Form1
         ' 
-        AutoScaleDimensions = New SizeF(10.0F, 25.0F)
+        AutoScaleDimensions = New SizeF(8.0F, 20.0F)
         AutoScaleMode = AutoScaleMode.Font
-        ClientSize = New Size(331, 562)
+        ClientSize = New Size(265, 450)
         Controls.Add(Label3)
         Controls.Add(Button5)
         Controls.Add(Button4)
-        Controls.Add(Button3)
-        Controls.Add(Button2)
+        Controls.Add(btnAplySettings)
+        Controls.Add(btnSaveHTML)
         Controls.Add(Label2)
         Controls.Add(Label1)
-        Controls.Add(Button1)
-        Margin = New Padding(4, 3, 4, 3)
+        Controls.Add(btnLoadMD)
+        Margin = New Padding(3, 2, 3, 2)
         Name = "Form1"
         Text = "md2flashcards"
         ResumeLayout(False)
         PerformLayout()
     End Sub
 
-    Friend WithEvents Button1 As Button
+    Friend WithEvents btnLoadMD As Button
     Friend WithEvents Label1 As Label
     Friend WithEvents Label2 As Label
-    Friend WithEvents Button2 As Button
-    Friend WithEvents Button3 As Button
+    Friend WithEvents btnSaveHTML As Button
+    Friend WithEvents btnAplySettings As Button
     Friend WithEvents Label3 As Label
     Friend WithEvents Button4 As Button
     Friend WithEvents Button5 As Button
+    Friend WithEvents opnFileDiag As OpenFileDialog
+    Friend WithEvents savFileDiag As SaveFileDialog
 End Class
--- md2flashcards/Main.vb
+++ md2flashcards/Main.vb
@@ -1,4 +1,8 @@
-﻿Public Class Form1
+﻿Imports System.ComponentModel
+
+Public Class Form1
+    Dim mdFileContent() As String
+    Dim mdFilePath As String
 
     Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
     End Sub
@@ -13,5 +17,26 @@
 
     Private Sub Label3_Click(sender As Object, e As EventArgs)
 
+    End Sub
+
+    Private Sub btnLoadMD_Click(sender As Object, e As EventArgs) Handles btnLoadMD.Click
+        opnFileDiag.ShowDialog()
+    End Sub
+
+    Private Sub btnSaveHTML_Click(sender As Object, e As EventArgs) Handles btnSaveHTML.Click
+
+    End Sub
+
+    Private Sub opnFileDiag_FileOk(sender As Object, e As CancelEventArgs) Handles opnFileDiag.FileOk
+        mdFilePath = opnFileDiag.FileName
+        btnSaveHTML.Enabled = True
+        btnAplySettings.Enabled = True
+    End Sub
+
+    Private Sub Form1_Paint(sender As Object, e As PaintEventArgs) Handles Me.Paint
+        If mdFilePath Is Nothing Then
+            btnSaveHTML.Enabled = False
+            btnAplySettings.Enabled = False
+        End If
     End Sub
 End Class
